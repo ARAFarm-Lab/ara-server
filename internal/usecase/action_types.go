4a38fa8485db21c0bfce1459c80fbba272b9a42f
Ibrahimsyah <ibra.himsyah10@gmail.com>
2024-03-27 13:15:43 +0700

    fix(json): fix inconsistency

--- internal/usecase/action_types.go
+++ internal/usecase/action_types.go
@@ -14,17 +14,17 @@ type ActionHistory struct {
 }
 
 type DispatcherAction struct {
-	ID   int64                `json:"id"`
-	Type constants.ActionType `json:"type"`
-	Name string               `json:"name"`
-	Icon string               `json:"icon"`
+	ID   int64                `json:"id,omitempty"`
+	Type constants.ActionType `json:"type,omitempty"`
+	Name string               `json:"name,omitempty"`
+	Icon string               `json:"icon,omitempty"`
 }
 
 type DispatcherParam struct {
-	DeviceID   int64       `json:"device_id"`
-	ActuatorID int64       `json:"actuator_id"`
-	Value      interface{} `json:"value"`
-	ActionBy   constants.ActionSource
+	DeviceID   int64                  `json:"device_id,omitempty"`
+	ActuatorID int64                  `json:"actuator_id,omitempty"`
+	Value      interface{}            `json:"value,omitempty"`
+	ActionBy   constants.ActionSource `json:"action_by,omitempty"`
 }
 
 type InsertActionLogParam struct {
